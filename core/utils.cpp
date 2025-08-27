d428a256aadd3cb776c53c830c2938f86b8616fd
Jiggly Balls <jigglyballs9000@gmail.com>
2025-08-27 21:00:04 +0530

    Made print function

---
+++ core/utils.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+
+
+void print(std::string text, int delay = 10000) {
+    // std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+
+    for (int index = 0; index < text.length(); index++)
+    {
+        std::cout << text[index];
+        Sleep(delay);
+    }
+    std::cout << std::endl;
+}
---
+++ core/utils.hpp
@@ -0,0 +1,8 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+#include <iostream>
+
+void print(std::string text, int delay = 10000);
+
+#endif
